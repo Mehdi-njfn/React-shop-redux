7d96f74c6f5558b23df334e117c70edda0247e92
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-03-07 23:48:07 +0330

    product data add

---
+++ src/productData.js
@@ -0,0 +1,36 @@
+import mobile from './assest/mobile.jpg'
+import camra from './assest/camra.jpg'
+import nokia from './assest/nokia.jpg'
+import phone from './assest/phone.jpg'
+
+
+export default [  
+    {
+      id: 1,
+      pName: 'Mobile',
+      price: 1000,
+      pImg: mobile,
+      count:0
+    },
+    {
+      id: 2,
+      pName: 'Camra',
+      price: 2000,
+      pImg: camra,
+      count:0
+    },
+    {
+      id: 3,
+      pName: 'Mobile',
+      price: 800,
+      pImg: phone,
+      count:0
+    },
+    {
+      id: 4,
+      pName: 'Nokia',
+      price: 500,
+      pImg: nokia,
+      count:0
+    },
+  ];
