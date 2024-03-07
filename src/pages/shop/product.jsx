1cc5f771148d2543c65cfe7524a8dc2bfee742e7
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-03-07 23:49:33 +0330

    product update

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,26 +1,59 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { shopContext } from "../../context/shopContext";
-
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, reduceOfCart, update } from '../../redux/cartSlice';
+import Aler from "../../aler";
 
 const Product = (props) => {
-  const { id, pName, pImg, price } = props.data;
-  // const {cartItems, add, remove}=useContext(shopContext);
-  // const isIncart = cartItems?.some((i)=>i.id===id);
+  const dispatch = useDispatch()
+  const cart = useSelector((state) => state.shopCart.cart);
 
-  // const  = cartItems?.filter((row)=> row.id===id)[0]?.count;
+  const product = props.data;
+
+  const isCart = props.c;
+
+  function tempAlert(msg, duration) {
+    var el = document.createElement("div");
+    el.setAttribute("style",
+      "position:absolute;top:1%;left:50%;background-color:#176bb5;font: bold;font-weight: 600;padding: 15px;color: white;border-radius: 5px;box-shadow: black;opacity: 100%;");
+    el.innerHTML = msg;
+    setTimeout(function () {
+      el.parentNode.removeChild(el);
+    }, duration);
+    document.body.appendChild(el);
+  }
+  function add() {
+    dispatch(addToCart(product));
+    if (!isCart) {
+      // alert(`${product.pName} added to cart`);
+      tempAlert(`${product.pName} added to cart`, 2500)
+    }
+  }
+  function reduce() {
+    dispatch(reduceOfCart(product));
+    if (!isCart) {
+      tempAlert(`${product.pName} reduce of cart`, 2500)
+    }
+  }
+  function isIncart() {
+    if (cart?.find((i) => i.id === product.id && i.count > 0)) {
+      return true;
+    } else return false;
+  }
+
   return (
     <>
       <div className="col-3">
-        <img className="w-100" src={pImg} alt={pImg} />
-        <h3>{pName}</h3>
-        <h5>price: {price}$</h5>
-        <button onClick={() => { }} className="btn btn-primary btn-sm">+</button>
-        <span className="mx-1">0</span>
-        {/*isIncart*/1 && <button onClick={() => { }} className="btn btn-primary btn-sm">-</button>}
-        <div className="my-2">price : price * count $</div>
+        <img className="w-100" src={product.pImg} alt={product.pImg} />
+        <h3>{product.pName}</h3>
+        <h5>price: {product.price}$</h5>
+        <button onClick={add} className="btn btn-success btn-sm ">+</button>
+        {isCart && <span className="mx-1">{product.count}</span>}
+        {isIncart() && <button onClick={reduce} className="btn btn-danger btn-sm">-</button>}
+        {isCart && <div className="my-2">price : {product.count * product.price} $</div>}
       </div>
     </>
-  );
+  )
 }
 
 export default Product;
