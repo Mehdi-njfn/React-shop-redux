c717c9da81177cca281bfdabfe52f9502bad398e
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-03-07 23:49:13 +0330

    cart jsx update

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,61 +1,50 @@
 import { useContext, useState } from "react";
 import { shopContext } from "../../context/shopContext";
 import Product from "../shop/product";
-import PRODUCT  from "../../data/products";
+import PRODUCT from "../../data/products";
+import { useDispatch, useSelector } from "react-redux";
+import { getCartTotal } from "../../redux/cartSlice";
 
 
 const Cart = () => {
-  // const {cartItems, reset} = useContext(shopContext);
+  const items = useSelector((state) => state.shopCart.cart)
+  const { cart, totalPrice, totalQuntity } = useSelector((state) => state.shopCart)
+  const dispatch = useDispatch();
 
-  // const itemsCount = cartItems?.reduce((prev, current)=>{
-  //   return prev + current.count;
-  // },0);
+  const isCart = true;
 
-  const list = {};
-
-//   const total =()=>{
-   
-//     PRODUCT.map((p)=>{
-//       if( cartItems?.some((i)=> i.id===p.id)){
-//         list.push(p.price)
-//     }
-//   }) 
-//   let a = list?.reduce((prev, current)=>{
-//     return prev + current;
-//   },0);
-//   console.log();
-  
-// }
-
-  return ( 
-    <>      
+  dispatch(getCartTotal());
+  return (
+    <>
       <h1>your cart:</h1>
       <div className="row ">
-        {/* {PRODUCT.map((p)=>{
-            if( cartItems?.some((i)=> i.id===p.id && i.count >0)){
-              return <Product key={p.id} data={p}></Product>
-            } 
-          })
+        {totalQuntity ? cart.map((p) => {
+          if (p.count > 0) {
+            return <Product key={p.id} data={p} c={isCart}></Product>
+          }
 
-        } */}
+        })
+        :<h2>cart is empty!</h2>
+        }
       </div>
       <hr />
       {/* <button className="btn btn-warning my-3" onClick={reset}>reset cart</button> */}
       <hr />
       <div className="container col-6">
-          <div className="card my-5 p-3">
-            {/* <h4>total : {itemsCount} products</h4> */}
-            <h3>total price : $</h3>
-            <div className="row ">
-              <button className="btn btn-success btn-lg mx-5 col-5">Buy</button>
+        <div className="card my-5 p-3">
+          {/* <h4>total : {itemsCount} products</h4> */}
+          <h3>total price :{totalPrice} </h3>
+          <h3>total Quantity :{totalQuntity} </h3>
+          <div className="row ">
+            <button className="btn btn-success btn-lg mx-5 col-5">Buy</button>
             <button className="btn btn-danger btn-lg  col-5">Cancel</button>
-              </div>
           </div>
+        </div>
       </div>
     </>
 
-   );
-   
+  );
+
 }
- 
+
 export default Cart;
