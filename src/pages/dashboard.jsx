f8e38094e56df82ce9bfee5c39e2b31c53dfc7d1
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-03-07 23:48:59 +0330

    dashboard update

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,16 +1,23 @@
+import Counter from '../counter';
+import PostList from '../postList';
+import PostsList from '../postsList';
 import { useContext } from "react";
 import { shopContext } from "../context/shopContext";
 
 
 
 const Dashboard = () => {
-  const {isAuth} = useContext(shopContext);
+  //const {isAuth} = useContext(shopContext);
 
-  return ( 
-    <>
-      <h1>dashboard</h1>
-    </>
-   );
+  return (
+    <div className="App " style={{marginInline:'auto', height:"100%"}}>
+      <Counter />
+      <br />
+      <PostList />
+      <br />
+      <PostsList />
+    </div>
+  );
 }
- 
+
 export default Dashboard;
