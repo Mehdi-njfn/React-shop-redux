c4be66fea5737e1fe6c2f5e042cd6cf02b4cc9b6
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-03-07 23:49:50 +0330

    shop update

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,13 +1,15 @@
+import { useSelector } from "react-redux";
 import PRODUCT from "../../data/products";
 import Product from "./product";
 
 
 const Shop = () => {
+  const items = useSelector((state)=>state.shopCart.items);
   return ( 
     <div className="container">
       <h1>Shop</h1>
       <div className="row">
-        {PRODUCT.map((p)=>{
+        {items.map((p)=>{
           return <Product data={p} key={p.id}/>
         })}
       </div>
